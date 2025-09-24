df46826ae3f585a6bb4b08aa5900a6d9ecf9f67c
muritador5050 <muritador5050@gmail.com>
2025-09-24 01:29:25 +0100

    i added manual download

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.jsx';
+import PwaInstaller from './components/PwaInstaller.tsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
+      <PwaInstaller />
       <App />
     </ChakraProvider>
   </StrictMode>
